277b24e22f6a39937dc52f778545fc4c877d6645
T1To48 <tofek.98@gmail.com>
2023-04-18 23:41:16 +0300

    Full PWA INTEGRATION

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,48 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { VitePWA } from 'vite-plugin-pwa'
 
 
 
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), VitePWA({ 
+    injectRegister: 'auto',
+   registerType: 'autoUpdate',
+  
+   workbox:{
+     globPatterns: ['**/*.{js,jsx,css,html,ico,png,svg}'],
+     cleanupOutdatedCaches: true
+   },
+   includeAssets:["favicon.ico","apple-touch-icon.png","mask-icon.png"],
+   manifest:{
+     name:"PECHOO Tracking APP",
+     short_name:"PECHOO",
+     description:"App that connects Restaurants with Freelancer Drivers",
+     theme_color:"#CD6D0C",
+     icons:[
+       {
+         src:"/icons/192x192-icon.png",
+         sizes:"192x192",
+         type:"image/png"
+       },
+       {
+         src:"/icons/512x512-icon.png",
+         sizes:"512x512",
+         type:"image/png"
+       },
+       {
+         src:"/icons/mask-icon.png",
+         sizes:"512x512",
+         type:"image/png",
+         purpose:"any maskable"
+       }
+     ]
+   }
+
+  })
+],
   build: {
     env: {
       VITE_BING_API_KEY: process.env.VITE_BING_API_KEY
